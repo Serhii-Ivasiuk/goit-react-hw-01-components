e0509b9ff1729c16b6f5b85fb1700f41210ab148
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-04-27 23:31:28 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -20,7 +20,7 @@ export function Statistics(props) {
 }
 
 Statistics.propTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired, // не впевнений чи це треба
